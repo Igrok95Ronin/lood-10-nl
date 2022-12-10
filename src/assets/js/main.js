b0e2283ccbdec547a06e66e10e7b416919a35f4a
xl92@DESKTOP-F8KSG17 <xl92@mail.ru>
2022-12-10 12:08:29 +0300

    finish adaptive

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,23 +8,16 @@ $('.slider__wrapper').slick({
     autoplaySpeed: 2000,
     responsive: [
         {
-          breakpoint: 1024,
+          breakpoint: 1025,
           settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToShow: 2,
+            slidesToScroll: 1,
             infinite: true,
             dots: true
           }
         },
         {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 480,
+          breakpoint: 768,
           settings: {
             slidesToShow: 1,
             slidesToScroll: 1
